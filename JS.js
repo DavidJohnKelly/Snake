108adf923855b3fd8f679cb17f471485ecf67196
David Kelly <davidkelly1507@gmail.com>
2021-02-26 10:18:22 +0000

    Added Random Apple Location Spawning

--- JS.js
+++ JS.js
@@ -1,6 +1,13 @@
-function GenerateApple(){
+function GenerateApple() {
+    var width = document.body.clientWidth - 40;
+    var height = document.documentElement.clientHeight - 40;
+    const coordinates = [(Math.floor(Math.random() * width)).toString(), (Math.floor(Math.random() * height)).toString()];
     var Appleimg = document.createElement("img");
     Appleimg.src = "C:/Users/wwwku/source/repos/Snake/Apple.png";
-    document.getElementById("PlayableArea").appendChild(Appleimg);
-    document.getElementById("StartButton").style.visibility = "hidden";
+
+    Appleimg.style.position = "absolute";
+    Appleimg.style.left = ((coordinates[0] + "px"));
+    Appleimg.style.top = ((coordinates[1]+ "px"));
+    document.body.appendChild(Appleimg);
+
 }
