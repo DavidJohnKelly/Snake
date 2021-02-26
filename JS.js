3be8d349a8ef7c8576003ffe4d1eb0d2e93cc2e7
David Kelly <davidkelly1507@gmail.com>
2021-02-26 11:05:21 +0000

    Apple OOP

--- JS.js
+++ JS.js
@@ -1,13 +1,18 @@
-function GenerateApple() {
-    var width = document.body.clientWidth - 40;
-    var height = document.documentElement.clientHeight - 40;
-    const coordinates = [(Math.floor(Math.random() * width)).toString(), (Math.floor(Math.random() * height)).toString()];
-    var Appleimg = document.createElement("img");
-    Appleimg.src = "C:/Users/wwwku/source/repos/Snake/Apple.png";
+var width = document.body.clientWidth - 40;
+var height = document.documentElement.clientHeight - 40;
+class Apple {
+    constructor(){
+        const width = document.body.clientWidth - 40;
+        const height = document.documentElement.clientHeight - 40;
+        const coordinates = [(Math.floor(Math.random() * width)).toString(), (Math.floor(Math.random() * height)).toString()];
+        var Appleimg = document.createElement("img");
+        Appleimg.src = "C:/Users/wwwku/source/repos/Snake/Apple.png";
 
-    Appleimg.style.position = "absolute";
-    Appleimg.style.left = ((coordinates[0] + "px"));
-    Appleimg.style.top = ((coordinates[1]+ "px"));
-    document.body.appendChild(Appleimg);
+        Appleimg.style.position = "absolute";
+        Appleimg.style.left = ((coordinates[0] + "px"));
+        Appleimg.style.top = ((coordinates[1] + "px"));
+        document.body.appendChild(Appleimg);
+    }
+    
 
 }
