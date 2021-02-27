e56c1cc5e7ff6fc0b9158e9b1a21126ec25d55af
David Kelly <davidkelly1507@gmail.com>
2021-02-27 08:35:05 +0000

    Initial Snake Classes

---
+++ SnakeClass.js
@@ -0,0 +1,24 @@
+class Body {
+    constructor() {
+        var Image = document.createElement("img");
+        Image.src = "C:/Users/wwwku/source/repos/Snake/Body.png";
+        Image.style.position = "absolute";
+    }
+}
+
+class Head extends Body {
+    constructor() {
+        super(Image) = document.createElement("img");
+        super(Image).src = "C:/Users/wwwku/source/repos/Snake/Head.png";
+        super(Image).style.position = "absolute";
+    }
+}
+
+class Tail extends Body {
+    constructor() {
+        super(Image) = document.createElement("img");
+        super(Image).src = "C:/Users/wwwku/source/repos/Snake/Tail.png";
+        super(Image).style.position = "absolute";
+    }
+}
+
