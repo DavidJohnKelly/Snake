0e061eaabadc957f4e742b89b83db7e52fae2f85
David Kelly <davidkelly1507@gmail.com>
2021-02-27 09:30:11 +0000

    Imported Images into code

--- MainGame.js
+++ MainGame.js
@@ -1,4 +1,7 @@
 function GameLoop() {
-
+    new Apple;
+    new SnakeSegment("Body");
+    new SnakeSegment("Head");
+    new SnakeSegment("Tail")
 }
 
--- SnakeClass.js
+++ SnakeClass.js
@@ -1,24 +1,17 @@
-class Body {
-    constructor() {
+class SnakeSegment {
+    constructor(type) {
+        this.type = type.toString();
+        var coordinates = {
+            x: (Math.floor(Math.random() * 150) + 10).toString(),
+            y: (Math.floor(Math.random() * 679) + 10).toString(),
+        };
         var Image = document.createElement("img");
-        Image.src = "C:/Users/wwwku/source/repos/Snake/Body.png";
+        Image.src = "C:/Users/wwwku/source/repos/Snake/"+this.type+".png";
         Image.style.position = "absolute";
+        Image.style.left = coordinates.x.toString() + "px";
+        Image.style.top = coordinates.y.toString() + "px";
+        document.body.appendChild(Image);
     }
 }
 
-class Head extends Body {
-    constructor() {
-        super(Image) = document.createElement("img");
-        super(Image).src = "C:/Users/wwwku/source/repos/Snake/Head.png";
-        super(Image).style.position = "absolute";
-    }
-}
-
-class Tail extends Body {
-    constructor() {
-        super(Image) = document.createElement("img");
-        super(Image).src = "C:/Users/wwwku/source/repos/Snake/Tail.png";
-        super(Image).style.position = "absolute";
-    }
-}
 
