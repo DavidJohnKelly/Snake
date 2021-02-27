781ac98faf80b421d2d2c40f7ca372a2599ae346
David Kelly <davidkelly1507@gmail.com>
2021-02-27 16:52:15 +0000

    Basic Snake Movement

--- MainGame.js
+++ MainGame.js
@@ -4,14 +4,47 @@ function GameStart() {
     var StartButton = document.getElementById("StartButton");
     StartButton.style.visibility = "hidden";
 
-    Snake[0]=new SnakeSegment("head", 300, 300);
-    Snake[1]=new SnakeSegment("body", 300, 300 - 46);
-    Snake[2]=new SnakeSegment("tail", 300, 300 - 92);
-    Snake[3].setType("body");
-    Snake[4] = new SnakeSegment("tail", 300, 300 - 92 - 46);
+    Snake[0]=new SnakeSegment("head", 300, 100);
+    Snake[1] = new SnakeSegment("body", 300, 100 - 46);
+    Snake[2] = new SnakeSegment("body", 300, 100 - 92);
+    Snake[3] = new SnakeSegment("body", 300, 100 - 92-46);
+    Snake[4] = new SnakeSegment("tail", 300, 100 - 92 - 46-46);
+    Snake[5] = new SnakeSegment("tail", 300, 100 - 92 - 46 - 46 - 46);
+    GameLoop()
 }
 
 function GameLoop() {
+    
+    //Movement();
+    Movement(); Movement(); Movement(); Movement(); Movement(); Movement(); Movement(); Movement(); Movement(); Movement(); Movement(); Movement(); Movement();
+
+        //sleep(100);
+    
+}
+
+function AddSegment() {
+    Snake[Snake.length - 1].setType("body");
+    var PreviousCoordinates = Snake[Snake.length - 1].getPosition();
+    Snake[Snake.length] = new SnakeSegment("tail", PreviousCoordinates.x, PreviousCoordinates.y - 46)
+
+}
+
+function sleep(miliseconds) {
+    var currentTime = new Date().getTime();
+    while (currentTime + miliseconds >= new Date().getTime()) {
+    }
+}
+
+function Movement() {
+    var HeadPosition = Snake[0].getPosition();
+    Snake[0].setPosition(HeadPosition.x, HeadPosition.y + 46);
+    Snake[Snake.length - 1].setPosition(HeadPosition.x, HeadPosition.y);
+    Snake[Snake.length - 1].setType("body");
+    Snake.splice(1, 0, Snake[Snake.length - 1]);
+    Snake.splice(Snake.length - 1, 1);
+
+    Snake[Snake.length - 1].setType("tail");
+
 
 }
 
