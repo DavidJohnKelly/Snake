98eee293d28ed260285c99cdaec5ceaab704e116
David Kelly <davidkelly1507@gmail.com>
2021-02-27 14:06:29 +0000

    Snake Component List

--- MainGame.js
+++ MainGame.js
@@ -1,7 +1,18 @@
-function GameLoop() {
-    new Apple;
-    new SnakeSegment("Body");
-    new SnakeSegment("Head");
-    new SnakeSegment("Tail")
+var Snake = [];
+
+function GameStart() {
+    var StartButton = document.getElementById("StartButton");
+    StartButton.style.visibility = "hidden";
+
+    Snake[0]=new SnakeSegment("head", 300, 300);
+    Snake[1]=new SnakeSegment("body", 300, 300 - 46);
+    Snake[2]=new SnakeSegment("tail", 300, 300 - 92);
+    Snake[3].setType("body");
+    Snake[4] = new SnakeSegment("tail", 300, 300 - 92 - 46);
 }
+
+function GameLoop() {
+
+}
+
 
--- SnakeClass.js
+++ SnakeClass.js
@@ -1,17 +1,41 @@
 class SnakeSegment {
-    constructor(type) {
-        this.type = type.toString();
-        var coordinates = {
-            x: (Math.floor(Math.random() * 150) + 10).toString(),
-            y: (Math.floor(Math.random() * 679) + 10).toString(),
+
+    constructor(type, x, y) {
+        this.type = type;
+        this.coordinates = {
+            x: x,
+            y: y,
         };
-        var Image = document.createElement("img");
-        Image.src = "C:/Users/wwwku/source/repos/Snake/"+this.type+".png";
-        Image.style.position = "absolute";
-        Image.style.left = coordinates.x.toString() + "px";
-        Image.style.top = coordinates.y.toString() + "px";
-        document.body.appendChild(Image);
+
+        this.Image = document.createElement("img");
+        this.Image.src = "C:/Users/wwwku/source/repos/Snake/" + type + ".png";
+        this.Image.style.position = "absolute";
+        this.Image.style.left = this.coordinates.x.toString() + "px";
+        this.Image.style.top = this.coordinates.y.toString() + "px";
+        document.body.appendChild(this.Image);
+    };
+
+    setPosition(xcoord, ycoord) {
+        this.coordinates = {
+            x: xcoord,
+            y: ycoord,
+        };
+        this.Image.style.left = this.coordinates.x.toString() + "px";
+        this.Image.style.top = this.coordinates.y.toString() + "px";
+    };
+
+    setType(SegmentType) {
+        this.type = SegmentType;
+        this.Image.src = "C:/Users/wwwku/source/repos/Snake/" + this.type + ".png";
+
     }
+
+    getPosition() {
+        return this.coordinates;
+    }
+
 }
+
+
 
 
