f333a39527382c45436b46d11aa1089bee7cf0f7
David Kelly <davidkelly1507@gmail.com>
2021-07-04 12:20:31 +0100

    Update AppleClass.js

--- AppleClass.js
+++ AppleClass.js
@@ -6,7 +6,7 @@ class Apple {
             y: ycoord.toString(),
         };
         this.Appleimg = document.createElement("img");
-        this.Appleimg.src = "C:/Users/wwwku/source/repos/Snake/Apple.png";
+        this.Appleimg.src = "Apple.png";
         this.Appleimg.style.position = "absolute";
         this.Appleimg.style.left = (this.coordinates.x + "px");
         this.Appleimg.style.top = (this.coordinates.y + "px");
